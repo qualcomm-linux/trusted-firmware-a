71f8545a4365011c4f258a403e2e3183dae85bbc
Sumit Garg <sumit.garg@oss.qualcomm.com>
2026-06-03 17:19:22 +0200

    feat(lemans_evk): enable power utils driver
    Enable support for the power utils driver init in the common BL31 init
    sequence and build the real power utils driver for Lemans EVK. The
    qti_pwr_utils_init() call is a no-op stub on SoCs that do not enable
    the driver.
    
    Signed-off-by: Sumit Garg <sumit.garg@oss.qualcomm.com>

--- plat/qti/common/src/qti_bl31_setup.c
+++ plat/qti/common/src/qti_bl31_setup.c
@@ -15,6 +15,7 @@
 #include <drivers/qti/accesscontrol/accesscontrol.h>
 #include <drivers/qti/accesscontrol/xpu.h>
 #include <drivers/qti/pdc/pdc.h>
+#include <drivers/qti/pwr_utils/pwr_utils.h>
 #include <drivers/qti/qtimer/qtimer.h>
 #include <drivers/qti/sec_core/sec_core.h>
 #include <drivers/qti/smmu/smmu.h>
@@ -102,6 +103,7 @@ void bl31_platform_setup(void)
 	plat_qti_gic_driver_init();
 	plat_qti_gic_init();
 	qti_pdc_init();
+	qti_pwr_utils_init();
 	qti_smmu_init();
 	qti_interrupt_svc_init(bl32_image_ep_info.pc != 0);
 	qti_sec_core_init();
--- plat/qti/hoya/lemans/lemans_evk/platform.mk
+++ plat/qti/hoya/lemans/lemans_evk/platform.mk
@@ -107,6 +107,7 @@ $(warning QTISECLIB_PATH is not provided while building, using stub implementati
 
 include drivers/qti/smmu/smmu.mk
 include drivers/qti/pdc/pdc.mk
+include drivers/qti/pwr_utils/pwr_utils.mk
 
 PLAT_INCLUDES   +=      -Iinclude/drivers/qti/qtimer/${CHIPSET} \
 			-Iinclude/drivers/qti/watchdog/${CHIPSET}
