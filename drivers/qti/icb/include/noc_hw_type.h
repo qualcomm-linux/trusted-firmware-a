a9a5e8f3bd00b170346b0f8437e13bc504cb1a0a
Navaneeth <npadhuri@qti.qualcomm.com>
2026-08-11 19:41:22 +0530

    feat(qti): add Nord NoC HW register descriptors
    Add the noc_hw/noc_sideband_hw/noc_msi_hw/noc_poc_hw register-offset
    descriptors required by the Nord NoC error back-end:
    
     - qnoc_sb_4_0, qnoc_sb_4_0_2, qnoc_sb_4_0_3, qnoc_sb_4_0_3l: sideband
       manager variants with one/two/three fault-in register pairs.
     - qnoc_msi_1_0: MSI encoder register offsets.
     - qnoc_poc_1_0_l: point-of-coherency variant without errloguser.
     - qnoc_poc_1_0_hscnoc_v2: point-of-coherency variant with errlogmisc
       (HSCNOC V2).
    
    Mark all descriptors in this header __unused, since each chipset
    back-end only references the subset of descriptors matching its own
    NoC IP revisions, and this header is shared across back-ends.
    
    Signed-off-by: Navaneeth <npadhuri@qti.qualcomm.com>

--- drivers/qti/icb/include/noc_hw_type.h
+++ drivers/qti/icb/include/noc_hw_type.h
@@ -6,11 +6,12 @@
 #ifndef QTI_NOC_HW_TYPE_H
 #define QTI_NOC_HW_TYPE_H
 
+#include <lib/libc/cdefs.h>
 #include <lib/utils_def.h>
 
 #include "noc_error.h"
 
-static struct noc_hw qnoc_4_0 = {
+static struct noc_hw __unused qnoc_4_0 = {
 	.swid_low       = 0x0,
 	.swid_high      = 0x4,
 	.main_ctl_low   = 0x8,
@@ -34,7 +35,7 @@ static struct noc_hw qnoc_4_0 = {
 };
 
 /* NOC SidebandManager HW Register Offsets */
-static struct noc_sideband_hw qnoc_sb_4_0_l = {
+static struct noc_sideband_hw __unused qnoc_sb_4_0_l = {
 	.swid_low             = 0x0,
 	.swid_high            = 0x4,
 	.faultin_en0_low      = 0x40,
@@ -51,7 +52,24 @@ static struct noc_sideband_hw qnoc_sb_4_0_l = {
 	.faultin_status2_high = REGISTER_NOT_APPLICABLE,
 };
 
-static struct noc_sideband_hw qnoc_sb_4_0_2l = {
+static struct noc_sideband_hw __unused qnoc_sb_4_0 = {
+	.swid_low             = 0x0,
+	.swid_high            = 0x4,
+	.faultin_en0_low      = 0x40,
+	.faultin_en0_high     = 0x44,
+	.faultin_status0_low  = 0x48,
+	.faultin_status0_high = 0x4C,
+	.faultin_en1_low      = REGISTER_NOT_APPLICABLE,
+	.faultin_en1_high     = REGISTER_NOT_APPLICABLE,
+	.faultin_status1_low  = REGISTER_NOT_APPLICABLE,
+	.faultin_status1_high = REGISTER_NOT_APPLICABLE,
+	.faultin_en2_low      = REGISTER_NOT_APPLICABLE,
+	.faultin_en2_high     = REGISTER_NOT_APPLICABLE,
+	.faultin_status2_low  = REGISTER_NOT_APPLICABLE,
+	.faultin_status2_high = REGISTER_NOT_APPLICABLE,
+};
+
+static struct noc_sideband_hw __unused qnoc_sb_4_0_2l = {
 	.swid_low             = 0x0,
 	.swid_high            = 0x4,
 	.faultin_en0_low      = 0x40,
@@ -68,8 +86,74 @@ static struct noc_sideband_hw qnoc_sb_4_0_2l = {
 	.faultin_status2_high = REGISTER_NOT_APPLICABLE,
 };
 
+static struct noc_sideband_hw __unused qnoc_sb_4_0_2 = {
+	.swid_low             = 0x0,
+	.swid_high            = 0x4,
+	.faultin_en0_low      = 0x40,
+	.faultin_en0_high     = 0x44,
+	.faultin_status0_low  = 0x48,
+	.faultin_status0_high = 0x4C,
+	.faultin_en1_low      = 0x50,
+	.faultin_en1_high     = 0x54,
+	.faultin_status1_low  = 0x58,
+	.faultin_status1_high = 0x5C,
+	.faultin_en2_low      = REGISTER_NOT_APPLICABLE,
+	.faultin_en2_high     = REGISTER_NOT_APPLICABLE,
+	.faultin_status2_low  = REGISTER_NOT_APPLICABLE,
+	.faultin_status2_high = REGISTER_NOT_APPLICABLE,
+};
+
+static struct noc_sideband_hw __unused qnoc_sb_4_0_3l = {
+	.swid_low             = 0x0,
+	.swid_high            = 0x4,
+	.faultin_en0_low      = 0x40,
+	.faultin_en0_high     = 0x44,
+	.faultin_status0_low  = 0x48,
+	.faultin_status0_high = 0x4C,
+	.faultin_en1_low      = 0x50,
+	.faultin_en1_high     = 0x54,
+	.faultin_status1_low  = 0x58,
+	.faultin_status1_high = 0x5C,
+	.faultin_en2_low      = 0x60,
+	.faultin_en2_high     = REGISTER_NOT_APPLICABLE,
+	.faultin_status2_low  = 0x68,
+	.faultin_status2_high = REGISTER_NOT_APPLICABLE,
+};
+
+static struct noc_sideband_hw __unused qnoc_sb_4_0_3 = {
+	.swid_low             = 0x0,
+	.swid_high            = 0x4,
+	.faultin_en0_low      = 0x40,
+	.faultin_en0_high     = 0x44,
+	.faultin_status0_low  = 0x48,
+	.faultin_status0_high = 0x4C,
+	.faultin_en1_low      = 0x50,
+	.faultin_en1_high     = 0x54,
+	.faultin_status1_low  = 0x58,
+	.faultin_status1_high = 0x5C,
+	.faultin_en2_low      = 0x60,
+	.faultin_en2_high     = 0x64,
+	.faultin_status2_low  = 0x68,
+	.faultin_status2_high = 0x6C,
+};
+
+/* NOC Msi HW Register Offsets */
+static struct noc_msi_hw __unused qnoc_msi_1_0 = {
+	.msienc_swid_low     = 0x0,
+	.msienc_swid_high    = 0x4,
+	.msienc_errorset_low = 0x8,
+	.msienc_errorsts_low = 0x10,
+	.msienc_errorclr_low = 0x18,
+	.msienc_errlog0_low  = 0x20,
+	.msienc_errlog0_high = 0x24,
+	.msienc_errlog1_low  = 0x28,
+	.msienc_errlog1_high = 0x2C,
+	.msienc_errlog2_low  = 0x30,
+	.msienc_errlog2_high = 0x34,
+};
+
 /* NOC Point-Of-Coherency HW Register Offsets - with errloguser */
-static struct noc_poc_hw qnoc_poc_1_0 = {
+static struct noc_poc_hw __unused qnoc_poc_1_0 = {
 	.swid_low        = 0x00,
 	.swid_high       = 0x04,
 	.errset_low      = 0x08,
@@ -83,6 +167,40 @@ static struct noc_poc_hw qnoc_poc_1_0 = {
 	.errloguser_high = 0x34,
 	.errlogmisc_low  = REGISTER_NOT_APPLICABLE,
 	.errlogmisc_high = REGISTER_NOT_APPLICABLE,
+};
+
+/* NOC Point-Of-Coherency HW Register Offsets - without errloguser */
+static struct noc_poc_hw __unused qnoc_poc_1_0_l = {
+	.swid_low        = 0x00,
+	.swid_high       = 0x04,
+	.errset_low      = 0x08,
+	.errstatus_low   = 0x10,
+	.errack_low      = 0x18,
+	.errlogmain_low  = 0x20,
+	.errlogmain_high = 0x24,
+	.errlogaddr_low  = 0x28,
+	.errlogaddr_high = 0x2C,
+	.errloguser_low  = REGISTER_NOT_APPLICABLE,
+	.errloguser_high = REGISTER_NOT_APPLICABLE,
+	.errlogmisc_low  = REGISTER_NOT_APPLICABLE,
+	.errlogmisc_high = REGISTER_NOT_APPLICABLE,
+};
+
+/* NOC Point-Of-Coherency HW Register Offsets - with errlogmisc (HSCNOC V2) */
+static struct noc_poc_hw __unused qnoc_poc_1_0_hscnoc_v2 = {
+	.swid_low        = 0x00,
+	.swid_high       = 0x04,
+	.errset_low      = 0x08,
+	.errstatus_low   = 0x10,
+	.errack_low      = 0x18,
+	.errlogmain_low  = 0x20,
+	.errlogmain_high = 0x24,
+	.errlogaddr_low  = 0x28,
+	.errlogaddr_high = 0x2C,
+	.errloguser_low  = REGISTER_NOT_APPLICABLE,
+	.errloguser_high = REGISTER_NOT_APPLICABLE,
+	.errlogmisc_low  = 0x30,
+	.errlogmisc_high = 0x34,
 };
 
 #endif /* QTI_NOC_HW_TYPE_H */
