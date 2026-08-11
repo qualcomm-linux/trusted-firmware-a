5229215255cc9ceb39ce3d12f2e2d57f14d5df8b
Navaneeth <npadhuri@qti.qualcomm.com>
2026-08-11 21:45:23 +0530

    feat(qti): add ICB micro-arbiter driver
    Add the common ICB (Interconnect Bus) micro-arbiter client API: create
    a client handle bound to an ICBID master/slave pair, submit bandwidth
    (AB/IB) requests, and destroy the client.
    
    The micro-arbiter is used to vote NoC bus-rail BCM (Bus Clock
    Manager) resources on via the RPMh command DB before NoC error
    registers can be safely programmed. include/drivers/qti/icb/icbid.h
    provides the enum icbid_master/icbid_slave identifiers consumed by
    this API, including the NSP0-3 process/HSC-NoC master and slave IDs
    used by Nord; the BCM command-DB resource layout used internally by
    the arbiter is described by the locally-defined struct cmd_db_bcm_aux
    in icbuarb.c.
    
    Signed-off-by: Navaneeth <npadhuri@qti.qualcomm.com>

---
+++ include/drivers/qti/icb/icbuarb.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2026, Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef QTI_ICBUARB_H
+#define QTI_ICBUARB_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <drivers/qti/icb/icbid.h>
+
+struct icb_bw_req {
+	uint64_t	ab;
+	uint64_t	ib;
+};
+
+/* Opaque per-client handle; NULL on failure. */
+typedef struct icb_client *icb_client_handle;
+
+bool qti_icbuarb_init(void);
+icb_client_handle icbuarb_create_client(enum icbid_master master,
+					enum icbid_slave slave);
+bool icbuarb_issue_request(icb_client_handle handle, struct icb_bw_req *req);
+bool icbuarb_destroy_client(icb_client_handle handle);
+
+#endif /* QTI_ICBUARB_H */
